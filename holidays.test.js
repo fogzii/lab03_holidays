0d245f41db9e56bbd20b9e860f535742e95e348d
Bowen Xiao <z516195@ad.unsw.edu.au>
2022-10-04 05:21:44 +1100

    added tests

--- holidays.test.js
+++ holidays.test.js
@@ -1,6 +1,14 @@
 import { holidaysInRange } from './holidays';
 
-test('Example - replace me', () => {
+test('Invalid start', () => {
+  expect(holidaysInRange(324, 1972)).toStrictEqual([]);
+});
+
+test('Invalid range', () => {
+  expect(holidaysInRange(1972, 324)).toStrictEqual([]);
+});
+
+test('General tests', () => {
   expect(holidaysInRange(1970, 1972)).toStrictEqual([
     {
       valentinesDay: 'Saturday, 14.02.1970',
@@ -16,6 +24,16 @@ test('Example - replace me', () => {
       valentinesDay: 'Monday, 14.02.1972',
       easter: 'Sunday, 02.04.1972',
       christmas: 'Monday, 25.12.1972',
+    }
+  ]);
+});
+
+test('0 year difference', () => {
+  expect(holidaysInRange(1970, 1970)).toStrictEqual([
+    {
+      valentinesDay: 'Saturday, 14.02.1970',
+      easter: 'Sunday, 29.03.1970',
+      christmas: 'Friday, 25.12.1970',
     }
   ]);
 });
